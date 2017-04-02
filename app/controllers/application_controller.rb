0ec2f29195ed32f1008d62d710399805d1c7a51f
mochi8k <y.hkh.185@gmail.com>
2017-04-02 12:25:04 +0000

    Chapter3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html:"hello,world!"
-  end
-
-  def goodbye
-    render html:"good,bye!"
-  end
 end
-
